7f50636c2cb307ff83fa3dbbc3464e45e35ac0f3
artturijalli <artturi.jalli@aalto.fi>
2019-11-16 14:03:39 +0200

    Initial commit

---
+++ GPSDatabaseTest/GPSDatabaseTest/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  GPSDatabaseTest
+//
+//  Created by Artturi Jalli on 12/11/2019.
+//  Copyright © 2019 Artturi Jalli. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import FirebaseDatabase
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation!
+    @IBOutlet weak var LocationsField: UILabel!
+    
+    @IBAction func UpdateLocation(_ sender: Any) {
+        updateDeviceLocationToDB()
+    }
+    
+    @IBAction func ShowLocations(_ sender: Any) {
+        showDetectedDevices()
+    }
+    
+    func getDeviceCurrentLocation() -> CLLocation{
+        let location = CLLocation()
+        return location
+    }
+    
+    func updateDeviceLocationToDB(){
+        let ref = Database.database().reference()
+        ref.child("DeviceData/\(UIDevice.current.name)").childByAutoId().setValue([
+            "latitude": locationManager.location?.coordinate.latitude as Any,
+            "longitude": locationManager.location?.coordinate.longitude as Any,
+            "timestamp": currentTime()
+        ])
+    }
+    
+    func currentTime() -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let myString = formatter.string(from: Date())
+        let yourDate = formatter.date(from: myString)
+        formatter.dateFormat = "dd-MMM-yyyy HH:mm"
+        let myStringafd = formatter.string(from: yourDate!)
+        return myStringafd
+    }
+    
+    func showDetectedDevices(){
+        let ref = Database.database().reference()
+        let productRef = ref.child("DeviceData")
+
+        productRef.observeSingleEvent(of: .value, with: { snapshot in
+            let locationDict: Dictionary<String,Dictionary<String,Any>>  = snapshot.value! as! Dictionary<String,Dictionary<String,Any>>
+            let device_names = locationDict.keys
+            var names_info = "Devices in the Database: \n"
+            for name in device_names{
+                names_info.append(name + "\n")
+            }
+            self.LocationsField.text = names_info
+        })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        getCurrentLocation()
+    }
+    
+    func getCurrentLocation(){
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+}
+
