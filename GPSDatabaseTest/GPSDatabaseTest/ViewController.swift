37ba2381806e03dd79e32df6608f8d4d243b55d9
artturijalli <artturi.jalli@aalto.fi>
2019-12-14 14:56:15 +0200

    Make code more readable

--- GPSDatabaseTest/GPSDatabaseTest/ViewController.swift
+++ GPSDatabaseTest/GPSDatabaseTest/ViewController.swift
@@ -19,31 +19,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var locationsData: Array<[String: Any]> = []
     @IBAction func UpdateLocation(_ sender: Any) {
-        updateDeviceLocationToDB()
+        updateLocationToDB()
     }
     var device_data_from_DB: [String: Any] = [:]
     @IBAction func ShowLocations(_ sender: Any) {
-        showDetectedDevices { (device_data) in
+        detectedDevices { (device_data) in
             self.device_data_from_DB = device_data as! [String : Any]
         }
-        let device_distance_info = showDistanceBetweenDevicesInDB(device_data: device_data_from_DB)
+        let device_distance_info = distancesBetweenDevicesInDB(device_data: device_data_from_DB)
         for info in device_distance_info{
             self.LocationsField.text! += info
         }
     }
 
-    func showDistanceBetweenDevicesInDB(device_data: [String: Any]) -> [String]{
+    func distancesBetweenDevicesInDB(device_data: [String: Any]) -> [String]{
     var distances: [[String]] = []
         for (origin_name, locations) in device_data {
             let locations_dict = locations as! [String: Double]
             let coords_origin: CLLocation = CLLocation(latitude: locations_dict["latitude"]!, longitude: locations_dict["longitude"]!)
             let distance_data = device_data.map { (arg0) -> String in
-                let (target_name, locs) = arg0
+                let (target_name, locations) = arg0
                 if target_name != origin_name{
-                    let loc_data = locs as! [String: Double]
-                    let lat = loc_data["latitude"]!
-                    let long = loc_data["longitude"]!
-                    let coords_target = CLLocation(latitude: lat, longitude: long)
+                    let loc_data = locations as! [String: Double]
+                    let latitude = loc_data["latitude"]!
+                    let longitude = loc_data["longitude"]!
+                    let coords_target = CLLocation(latitude: latitude, longitude: longitude)
                     return "\(origin_name) - \(target_name): \(Double(distance(loc1: coords_origin, loc2: coords_target)).rounded())m \n"
                 }
                 return ""
@@ -53,39 +53,32 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         return distances.flatMap{ $0 }
     }
     
-    func getDeviceCurrentLocation() -> CLLocation{
-        let location = CLLocation()
-        return location
-    }
-    
-    func updateDeviceLocationToDB(){
+    func updateLocationToDB(){
         let ref = Database.database().reference()
         ref.child("DeviceData/\(UIDevice.current.name)").setValue(locationsData)
     }
     
-    func currentTime() -> String{
+    func getCurrentTime() -> String{
         let formatter = ISO8601DateFormatter()
         return formatter.string(from: Date())
     }
     
-    func showDetectedDevices(completion: @escaping (_ result: Any) -> Void) -> [String: Any]{
+    func detectedDevices(completion: @escaping (_ result: Any) -> Void) -> [String: Any]{
         var all_data: [String: Any] = [:]
-        let ref = Database.database().reference()
-        let productRef = ref.child("DeviceData")
-        productRef.observe(.childAdded) { (snapshot) in
+        let database_reference = Database.database().reference()
+        let productRef = database_reference.child("DeviceData")
+        productRef.observe(.childAdded) { (device_data) in
             DispatchQueue.main.async {
-                for child in snapshot.children{
-                    let snap = child as! DataSnapshot
-                    var loc_data: [String: Double] = [:]
-                    for elem in snap.children{
-                        var data = elem as! DataSnapshot
-                        if data.key != "timestamp"{
-                            let d = data.value as! Double
-                            loc_data["\(data.key)"] = d
+                for location_element in device_data.children{
+                    var location_data: [String: Double] = [:]
+                    for location in (location_element as! DataSnapshot).children{
+                        let device_location_data = location as! DataSnapshot
+                        if device_location_data.key != "timestamp"{
+                            location_data["\(device_location_data.key)"] = device_location_data.value as? Double
                         }
                     }
-                    if loc_data != [:]{
-                        all_data["\(snapshot.key)"] = loc_data
+                    if location_data != [:]{
+                        all_data["\(device_data.key)"] = location_data
                     }
                 }
                 completion(all_data)
@@ -105,7 +98,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             self.locationsData.append([
                 "latitude": self.locationManager.location?.coordinate.latitude as Any,
                 "longitude": self.locationManager.location?.coordinate.longitude as Any,
-                "timestamp": self.currentTime()
+                "timestamp": self.getCurrentTime()
             ])
         }
     }
