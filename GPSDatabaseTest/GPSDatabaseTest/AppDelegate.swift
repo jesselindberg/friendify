c9c4cbecc68d1bebd971fc7b9fa7ba0d6dbb9863
artturijalli <artturi.jalli@aalto.fi>
2020-01-22 19:37:55 +0200

    Add simple mail authentication login

--- GPSDatabaseTest/GPSDatabaseTest/AppDelegate.swift
+++ GPSDatabaseTest/GPSDatabaseTest/AppDelegate.swift
@@ -9,6 +9,9 @@
 import UIKit
 import Firebase
 
+let userDefault = UserDefaults.standard
+let launchedBefore = UserDefaults.standard.bool(forKey: "usersignedin")
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
