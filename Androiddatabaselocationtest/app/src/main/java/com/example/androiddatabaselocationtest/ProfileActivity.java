6b0f93d31ee2fd50a16e78ab226ce6b70caec117
ViliMer <vili.merilainen@hotmail.com>
2020-03-21 18:49:12 +0200

    Almost complete profile page functionality

--- Androiddatabaselocationtest/app/src/main/java/com/example/androiddatabaselocationtest/ProfileActivity.java
+++ Androiddatabaselocationtest/app/src/main/java/com/example/androiddatabaselocationtest/ProfileActivity.java
@@ -1,26 +1,256 @@
 package com.example.androiddatabaselocationtest;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.signature.ObjectKey;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private DatabaseReference mUserReference;
+    private final int PICK_IMAGE_REQUEST = 22;
+    private Uri filePath;
+    String uID;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        final ImageView profileImage = (ImageView) findViewById(R.id.profileImage);
+        Button changeImageButton = (Button) findViewById(R.id.changeImageBtn);
+        Button saveButton = (Button) findViewById(R.id.profileSaveBtn);
+        final TextView nameField = (TextView) findViewById(R.id.txtViewName);
+        final EditText firstNameField = (EditText) findViewById(R.id.editTxtFirstName);
+        final EditText lastNameField = (EditText) findViewById(R.id.editTxtLastName);
+        final EditText nicknameField = (EditText) findViewById(R.id.editTxtNickname);
+        final EditText infoField = (EditText) findViewById(R.id.editTxtInfo);
+
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
-        System.out.println(user.getUid());
+        uID = user.getUid();
+        uID = "GWANMHWhZMWFO2qPOU4eEZiOnt43";
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Get user image
+        final StorageReference storageReference = FirebaseStorage.getInstance("gs://ioslocationdatabase.appspot.com").getReference().child("profile/" + uID);
+        storageReference.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
+            @Override
+            public void onSuccess(StorageMetadata storageMetadata) {
+                GlideApp.with(ProfileActivity.this)
+                        .load((storageReference))
+                        .placeholder(R.drawable.profile_round)
+                        .signature(new ObjectKey(storageMetadata.getUpdatedTimeMillis()))
+                        .into(profileImage);
+            }
+        });
+
+
+        // Get user data
+        mUserReference = FirebaseDatabase.getInstance().getReference().child("users/" + uID);
+        ValueEventListener userDataListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                nameField.setText(user.f_name + " " + user.l_name);
+                firstNameField.setText(user.f_name);
+                lastNameField.setText(user.l_name);
+                nicknameField.setText(user.username);
+                infoField.setText(user.info);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mUserReference.addValueEventListener(userDataListener);
+
+        // Select image
+        changeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        // Save data
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = firstNameField.getText().toString();
+                String lastName = lastNameField.getText().toString();
+                String nickname = nicknameField.getText().toString();
+                String info = infoField.getText().toString();
+
+                mUserReference.child("f_name").setValue(firstName);
+                mUserReference.child("l_name").setValue(lastName);
+                mUserReference.child("username").setValue(nickname);
+                mUserReference.child("info").setValue(info);
+
+                uploadImage();
+            }
+        });
     }
+
+
+    // Select Image method
+    private void selectImage()
+    {
+
+        // Defining Implicit Intent to mobile gallery
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                PICK_IMAGE_REQUEST);
+    }
+
+
+    // Override onActivityResult method
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // checking request code and result code
+        // if request code is PICK_IMAGE_REQUEST and
+        // resultCode is RESULT_OK
+        // then set image in the image view
+        if (requestCode == PICK_IMAGE_REQUEST
+                && resultCode == RESULT_OK
+                && data != null
+                && data.getData() != null) {
+
+            // Get the Uri of data
+            filePath = data.getData();
+            try {
+
+                // Setting image on image view using Bitmap
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                ImageView profileImage = (ImageView) findViewById(R.id.profileImage);
+                profileImage.setImageBitmap(bitmap);
+            }
+            catch (IOException e) {
+                // Log the exception
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // UploadImage method
+    private void uploadImage()
+    {
+        if (filePath != null) {
+
+            // Code for showing progressDialog while uploading
+            final ProgressDialog progressDialog
+                    = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            // Defining the child of storageReference
+            StorageReference ref = FirebaseStorage
+                    .getInstance("gs://ioslocationdatabase.appspot.com")
+                    .getReference().child("profile/" + uID);
+
+            // adding listeners on upload
+            // or failure of image
+            ref.putFile(filePath)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                                @Override
+                                public void onSuccess(
+                                        UploadTask.TaskSnapshot taskSnapshot)
+                                {
+
+                                    // Image uploaded successfully
+                                    // Dismiss dialog
+                                    progressDialog.dismiss();
+                                    Toast
+                                            .makeText(ProfileActivity.this,
+                                                    "Profile Updated!!",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
+                            })
+
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e)
+                        {
+
+                            // Error, Image not uploaded
+                            progressDialog.dismiss();
+                            Toast
+                                    .makeText(ProfileActivity.this,
+                                            "Failed " + e.getMessage(),
+                                            Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    })
+                    .addOnProgressListener(
+                            new OnProgressListener<UploadTask.TaskSnapshot>() {
+
+                                // Progress Listener for loading
+                                // percentage on the dialog box
+                                @Override
+                                public void onProgress(
+                                        UploadTask.TaskSnapshot taskSnapshot)
+                                {
+                                    double progress
+                                            = (100.0
+                                            * taskSnapshot.getBytesTransferred()
+                                            / taskSnapshot.getTotalByteCount());
+                                    progressDialog.setMessage(
+                                            "Uploaded "
+                                                    + (int)progress + "%");
+                                }
+                            });
+        }
+    }
+
+
 }
--- Androiddatabaselocationtest/app/src/main/java/com/example/androiddatabaselocationtest/User.java
+++ Androiddatabaselocationtest/app/src/main/java/com/example/androiddatabaselocationtest/User.java
@@ -1,34 +1,22 @@
 package com.example.androiddatabaselocationtest;
 
-import android.media.Image;
-
-import com.bumptech.glide.Glide;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
 
 public class User {
-    public String firstName;
-    public String lastName;
-    public String nick;
-    public String uID;
-    public Image profileImage;
+    public String f_name;
+    public String l_name;
+    public String username;
+    public String info;
 
     public User() {
         // Default constructor required for calls to DataSnapshot.getValue(CurrentLocation.class)
     }
 
-    public User(String firstName, String lastName, String nick, String uID) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.nick = nick;
-        this.uID = uID;
-        fetchImage(uID);
+    public User(String firstName, String lastName, String nick, String info) {
+        this.f_name = firstName;
+        this.l_name = lastName;
+        this.username = nick;
+        this.info = info;
     }
 
-    public void fetchImage(String uID){
-        StorageReference storageReference = FirebaseStorage.getInstance("gs://ioslocationdatabase.appspot.com").getReference().child(uID);
-        MyAppGlideModule.with(this).load(storageReference).into();
 
-    }
 }
